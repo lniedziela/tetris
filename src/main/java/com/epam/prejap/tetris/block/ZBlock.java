58114c3e8036e06fa6a0d736c95a4d31501bc8b5
Lukasz Niedziela <lukaszniedzie@gmail.com>
2021-06-25 08:03:19 +0200

    4. Changes w.r.t. Nikita changes.

--- src/main/java/com/epam/prejap/tetris/block/ZBlock.java
+++ src/main/java/com/epam/prejap/tetris/block/ZBlock.java
@@ -8,6 +8,14 @@ package com.epam.prejap.tetris.block;
  */
 final class ZBlock extends Block {
 
+    /**
+     * Byte array represents "Z" block.
+     * In game will be displayed as (between lines):
+     *  -------------------
+     *      ##
+     *       ##
+     *  -------------------
+     */
     private static final byte[][] IMAGE = {
             {1, 1, 0},
             {0, 1, 1}
